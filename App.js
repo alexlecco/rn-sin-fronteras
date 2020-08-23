2f6948f63b51485f9c146225268b44a8aec774da
alexlecco <alexlecco24@gmail.com>
2020-08-23 02:04:44 -0300

    implement local and remote images

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { StyleSheet, ActivityIndicator, View } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="large" color="#0000ff"/>
+      <Image
+        style={styles.photo}
+        source={require('./assets/icon.png')}
+      />
+      <Image
+        style={styles.photo}
+        source={{ uri: 'http://placekitten.com/200/200' }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  photo: {
+    height: 200,
+    width: 200,
+    marginBottom: 20,
+  },
   container: {
     flex: 1,
-    alignItems: 'stretch',
+    alignItems: 'center',
     justifyContent: 'center',
   },
 });
