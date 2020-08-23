499f711a2c0b420c8bfe67301a64eec0b4a41d7c
alexlecco <alexlecco24@gmail.com>
2020-08-23 10:17:55 -0300

    implement useEffect

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 
 export default function App() {
   const [ count, setCount ] = useState(0)
+  const [ loading, setLoading ] = useState(true)
   const increment = () => setCount(count + 1)
   const decrement = () => setCount(count - 1)
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>"{count}"</Text>
-      <Button title="incrementar" onPress={increment} />
-      <Button title="incrementar" onPress={decrement} />
+      {
+        loading ?
+          <Text>Cargando...</Text>
+        :
+          <View>
+            <Text style={styles.text}>"{count}"</Text>
+            <Button title="incrementar" onPress={increment} />
+            <Button title="incrementar" onPress={decrement} />
+          </View>
+      }
     </View>
   );
 }
@@ -23,5 +37,6 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+    textAlign: 'center'
   },
 });
