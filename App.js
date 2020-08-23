1a7112a8a339c0676b4dc2b4fcc4e0364173691f
alexlecco <alexlecco24@gmail.com>
2020-08-23 10:04:25 -0300

    implement useState

--- App.js
+++ App.js
@@ -1,50 +1,27 @@
 import React, { useState } from 'react';
-import { StyleSheet, Modal, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 export default function App() {
-  const [ modal, setModal ] = useState(false)
-  const interactModal = () => setModal(!modal)
+  const [ count, setCount ] = useState(0)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
 
   return (
     <View style={styles.container}>
-      <Modal
-        animatinType="slide"
-        transparent={true}
-        visible={modal}
-        onRequestClose={() => {}}
-      >
-        <View style={styles.center}>
-          <View style={styles.content}>
-            <Text>Soy un modal</Text>
-            <Button title="Cerrar modal" onPress={interactModal} />
-          </View>
-        </View>
-      </Modal>
-
-      <Text>No soy un modal</Text>
-      <Button title="Abrir modal" onPress={interactModal} />
-      
+      <Text style={styles.text}>"{count}"</Text>
+      <Button title="incrementar" onPress={increment} />
+      <Button title="incrementar" onPress={decrement} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  content: {
-    backgroundColor: '#fff',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 25,
-  },
-  center: {
-    flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.3)'
-  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
   },
 });
