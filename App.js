d7ea027bf14ee32031eaa295b7252f7bfe75ca4b
alexlecco <alexlecco24@gmail.com>
2020-08-23 01:57:43 -0300

    use ActivityIndicator as a loader+

--- App.js
+++ App.js
@@ -1,52 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React from 'react';
+import { StyleSheet, ActivityIndicator, View } from 'react-native';
 
 export default function App() {
-  const [ users, setUsers ] = useState([])
-  const [ loading, setLoading ] = useState(true)
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => {
-        setUsers(data)
-        setLoading(false)
-        console.log(data)
-      })
-  }, [])
-
-  if(loading) {
-    return <View style={styles.center}><Text>Cargando...</Text></View>
-  }
-
   return (
     <View style={styles.container}>
-      <FlatList
-        data={users}
-        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text> }
-        keyExtractor={item => String(item.id)}
-      />
+      <ActivityIndicator size="large" color="#0000ff"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   container: {
     flex: 1,
     alignItems: 'stretch',
     justifyContent: 'center',
-    paddingTop: 22,
-  },
-  item: {
-    padding: 10,
-    fontSize: 22,
-    height: 50,
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1,
   },
 });
