092d252b887a6ff7059d5bd52debc2f7eac27aa0
alexlecco <alexlecco24@gmail.com>
2020-08-23 09:43:04 -0300

    implement Modal component

--- App.js
+++ App.js
@@ -1,30 +1,46 @@
-import React from 'react';
-import { StyleSheet, ImageBackground, View, Text, Dimensions } from 'react-native';
-
-const deviceHeight = Dimensions.get('window').height
-const deviceWidth = Dimensions.get('window').width
-
-console.log(deviceHeight)
-console.log(deviceWidth)
+import React, { useState } from 'react';
+import { StyleSheet, Modal, View, Text, Button } from 'react-native';
 
 export default function App() {
+  const [ modal, setModal ] = useState(false)
+  const interactModal = () => setModal(!modal)
+
   return (
     <View style={styles.container}>
-      <ImageBackground
-        style={styles.photo}
-        source={{ uri: 'http://placekitten.com/700/360' }}
+      <Modal
+        animatinType="slide"
+        transparent={true}
+        visible={modal}
+        onRequestClose={() => {}}
       >
-        <Text style={{ color: 'white' }}>Mi aplicación</Text>
-      </ImageBackground>
+        <View style={styles.center}>
+          <View style={styles.content}>
+            <Text>Soy un modal</Text>
+            <Button title="Cerrar modal" onPress={interactModal} />
+          </View>
+        </View>
+      </Modal>
+
+      <Text>No soy un modal</Text>
+      <Button title="Abrir modal" onPress={interactModal} />
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  photo: {
-    height: deviceHeight,
-    width: deviceWidth,
-    marginBottom: 20,
+  content: {
+    backgroundColor: '#fff',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 25,
+  },
+  center: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.3)'
   },
   container: {
     flex: 1,
