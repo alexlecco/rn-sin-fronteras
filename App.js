13a04d1ccda6c0fd42b38467f7425bc2c28b0877
alexlecco <alexlecco24@gmail.com>
2020-08-24 17:40:15 -0300

    implement useCallback hook

--- App.js
+++ App.js
@@ -1,62 +1,26 @@
-import React, { useReducer, useMemo } from 'react';
+import React, { useState, useCallback } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-// actions names
-const INCREMENT = 'INCREMENT'
-const DECREMENT = 'DECREMENT'
-
-// initial state
-const initialState = {
-  count: 0
-}
-
-// reducer
-const reducer = (state, action) => {
-  switch(action.type) {
-    case INCREMENT: {
-      return { count: state.count + 1 }
-    }
-    case DECREMENT: {
-      return { count: state.count - 1 }
-    }
-    default: {
-      return state
-    }
-  }
-}
-
-const users = [
-  {
-    name: 'Mario',
-    age: 30,
-  },
-  {
-    name: 'Luigi',
-    age: 28,
-  }
-]
-
 export default function App() {
-  const [ state, dispatch ] = useReducer(reducer, initialState)
-  const totalAge = useMemo(() => {
-    let age = 0
+  const [ count, setCount ] = useState(0)
 
-    console.log('calculando...')
+  const increment = useCallback(
+    () => {
+      setCount(count + 1)
+    }, [count]
+  )
 
-    users.forEach(x => {
-      age = age + x.age
-    })
-
-    return age
-  }, [users])
-
-  console.log('edad total: ', totalAge)
+  const decrement = useCallback(
+    () => {
+      setCount(count - 1)
+    }, [count]
+  )
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text} onPress={() => dispatch({ type: INCREMENT })}> + </Text>
-      <Text style={styles.text}> {state.count} </Text>
-      <Text style={styles.text} onPress={() => dispatch({ type: DECREMENT })}> - </Text>
+      <Text style={styles.text} onPress={increment}> + </Text>
+      <Text style={styles.text}> {count} </Text>
+      <Text style={styles.text} onPress={decrement}> - </Text>
     </View>
   );
 }
