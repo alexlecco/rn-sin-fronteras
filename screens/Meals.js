36a9466eb403dc5fe7589ec9ea16fb25a8393dbb
alexlecco <alexlecco24@gmail.com>
2020-09-09 20:47:42 -0300

    create meals list

--- screens/Meals.js
+++ screens/Meals.js
@@ -1,10 +1,47 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { StyleSheet, View, FlatList } from 'react-native'
+import ListItem from '../components/ListItem'
 
-export default () => {
+const data = [
+  {
+    _id: 'lala',
+    name: 'Churrasco',
+    description: "Plato típico! con palta y mayonesa"
+  }
+]
+
+const Meals = ({ navigation }) => {
   return(
-    <View>
-      <Text>Soy Meals</Text>
+    <View style={styles.container}>
+      <FlatList 
+        style={styles.list}
+        data={data}
+        keyExtractor={x => x._id}
+        renderItem={({ item }) => 
+          <ListItem
+            onPress={() => navigation.navigate('Modal', { _id: item._id })}
+            name={item.name}
+          />
+        }
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start'
+  },
+  list: {
+    alignSelf: 'stretch'
+  }
+})
+
+Meals.navigationOptions = ({
+  title: 'Comidas disponibles'
+})
+
+export default Meals
--- screens/Modal.js
+++ screens/Modal.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 
 export default () => {
   return(
-    <View>
+    <View style={styles.container}>
       <Text>Soy un Modal</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
