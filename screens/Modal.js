1254121b77ed29b010891fae832b65ce5889c84b
alexlecco <alexlecco24@gmail.com>
2020-09-10 02:29:32 -0300

    create details Modal for each meal

--- screens/Modal.js
+++ screens/Modal.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Image } from 'react-native'
+import useFetch from '../hooks/useFetch'
 
-export default () => {
+export default ({ navigation }) => {
+  const id = navigation.getParam('_id')
+  const { loading, data } = useFetch(`https://serverless.alexlecco.vercel.app/api/meals/${id}`)
+  console.log("data:::", data)
+
   return(
     <View style={styles.container}>
-      <Text>Soy un Modal</Text>
+      {
+        loading ?
+          <Text>Cargando...</Text>
+        :
+          <>
+            <Text> {data.name} </Text>
+            <Image
+              style={styles.image}
+              source={{
+                uri: data.image,
+              }}
+            />
+            <Text> {data.description} </Text>
+          </>
+      }
     </View>
   )
 }
@@ -14,5 +33,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  }
+  },
+  image: {
+    width: 200,
+    height: 150,
+  },
 })
